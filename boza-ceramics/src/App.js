e03319c5878aa4f1661a97f0d5b2b5a53307b964
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-13 13:57:57 +0100

    installed the bootstrap

---
+++ boza-ceramics/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="btn btn-primary">Hello </span>
+    </div>
+  );
+}
+
+export default App;
